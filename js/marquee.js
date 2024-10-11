ce79b62c75d7414bca977074bff014128622c16b
Cecilie Jasmin Jørgensen <ceci919p@stud.kea.dk>
2024-10-11 10:05:18 +0200

    changes to marquee

--- js/marquee.js
+++ js/marquee.js
@@ -1,4 +1,4 @@
-const marqueeContent = document.querySelector(".marquee-content");
+const marqueeContents = document.querySelectorAll(".marquee-content");
 const baseSpeed = 30; // base speed in seconds for larger screens
 const mobileSpeed = 60; // speed for mobile screens
 
@@ -8,18 +8,19 @@ function adjustMarqueeSpeed() {
   // Set animation speed based on screen size
   let calculatedSpeed = baseSpeed;
   if (screenWidth < 768) {
-    // Change this breakpoint as needed
     calculatedSpeed = mobileSpeed; // slower speed for mobile devices
   } else {
     calculatedSpeed = Math.max(baseSpeed * (screenWidth / 1200), 5); // minimum speed of 5s
   }
 
-  marqueeContent.style.animationDuration = `${calculatedSpeed}s`;
+  marqueeContents.forEach((content) => {
+    content.style.animationDuration = `${calculatedSpeed}s`;
 
-  // After the first iteration, set the animation to start from 100%
-  setTimeout(() => {
-    marqueeContent.style.animationDelay = `${calculatedSpeed}s`; // start from 100%
-  }, calculatedSpeed * 1000); // convert seconds to milliseconds
+    // After the first iteration, set the animation to start from 100%
+    setTimeout(() => {
+      content.style.animationDelay = `${calculatedSpeed}s`; // start from 100%
+    }, calculatedSpeed * 1000); // convert seconds to milliseconds
+  });
 }
 
 // Adjust speed on resize and load
